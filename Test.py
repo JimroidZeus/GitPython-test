35413a3e6c826f8ca0f08569cccf166141261a4a
JimroidZeus <colinmacpherson@gmail.com>
2012-08-09 17:37:10 -0400

    Cleaned up

--- Test.py
+++ Test.py
@@ -1,3 +1,5 @@
 import os
 
 print 'This is a test file'
+
+#Doing the same thing here as I did in test2.py
--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
 import os
 
 print 'Just another test file'
+
+#adding some comments so git thinks it's dirty.
